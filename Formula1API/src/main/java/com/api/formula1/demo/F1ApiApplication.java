14c392961661ad57969cf86ea150e90650f9aeef
Chris King <cking5836@gmail.com>
2021-04-10 16:34:39 -0400

    Ran Spring init

---
+++ Formula1API/src/main/java/com/api/formula1/demo/F1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.formula1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1ApiApplication.class, args);
+    }
+
+}
